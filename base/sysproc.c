383edbc7cd0aded71b8dba26e2e796ec8ef7344c
Matt Murphy <mmurph56@binghamton.edu>
2023-09-14 13:09:22 -0400

    error with halt2

--- base/sysproc.c
+++ base/sysproc.c
@@ -89,3 +89,11 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_halt(void)
+{
+  outw(0xB004, 0x0|0x2000);
+  outw(0x604, 0x0|0x2000);
+  return 0;
+}
--- base/user.h
+++ base/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int halt2(void);
 
 // ulib.c
 int stat(char*, struct stat*);
